7db4f0e688c1ee4d768ccf727126660edb690579
paul <pmc.valentin@gmail.com>
2016-03-22 17:20:32 +0100

    all

--- lib.h
+++ lib.h
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
 
 #include <sys/time.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+
+#include <termios.h>
--- minish.c
+++ minish.c
@@ -1,16 +1,56 @@
 #include "lib.h"
 
+int readkey();
 int main(int argc, char **argv){
-	
+	char buffer[1024];
+	char *separator = { " " };
+	pid_t pidF;
+	int key = 1;
+	int i=0;
 	while(key != '\0'){
-		key = readkey();
+		char *token ;
+		char **charac;
+		char *save;
+		read(STDIN_FILENO, buffer,1024);
+		token = strtok_r(buffer,separator,&save);
+
+		while(token != NULL){
+			printf("TOKEN = %s\n ", token);
+			*(charac+i) = token;
+			//token = '\0';
+			i++;
+			token = strtok_r(NULL, separator,&save);
+		}
+
 		switch(pidF = fork()){
 			case -1:
 				perror("Fork failed");
 				return 1;
 			case 0:
+				if(execvp(charac[0], charac) == -1){
+					perror("Commande non valide");
+					return 2;
+				}
+		}
+		while(waitpid(pidF,0,0) < 0);
+	}
 
 
 
 	return 0;
+}
+
+int readkey(){
+	struct termios init;
+	struct termios config;
+	int key;
+	tcgetattr(STDIN_FILENO, &init);
+	config = init;
+	config.c_lflag &= ~ICANON;
+	config.c_lflag &= ~ECHO;
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &config);
+	key = getchar();
+	tcsetattr(STDIN_FILENO, TCSANOW, &init);
+	return key;
 }
